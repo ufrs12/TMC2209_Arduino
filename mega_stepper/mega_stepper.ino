4e743858f02ef8687729e7fb46d0a7ba47267bb5
ufrs12 <ufrs12@yandex.ru>
2025-10-13 19:55:56 +0300

    1

--- mega_stepper/mega_stepper.ino
+++ mega_stepper/mega_stepper.ino
@@ -1,3 +1,14 @@
+//  Карта Modbus-регистров
+//
+//  Регистр | Значение
+//     0    | Уставка тока драйвера
+//     1    | Текущий ток драйвера
+//     2    | Максимальная скорость
+//     3    | Максимальное ускорение
+//    10    | Текущаяя позиция
+//    11    | Дистанция до цели
+//    12    | Цель
+
 #include <ModbusRTUSlave.h>                         //  
 #define MODBUS_SERIAL Serial1                       //  ███╗░░░███╗░█████╗░██████╗░██████╗░██╗░░░██╗░██████╗
 #define MODBUS_BAUD 115200                          //  ████╗░████║██╔══██╗██╔══██╗██╔══██╗██║░░░██║██╔════╝
@@ -5,7 +16,7 @@
 #define MODBUS_UNIT_ID 1                            //  ██║╚██╔╝██║██║░░██║██║░░██║██╔══██╗██║░░░██║░╚═══██╗
 const int16_t dePin = 10;                           //  ██║░╚═╝░██║╚█████╔╝██████╔╝██████╦╝╚██████╔╝██████╔╝
 ModbusRTUSlave modbus(MODBUS_SERIAL, dePin);        //  ╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═════╝░░╚═════╝░╚═════╝░
-const uint8_t numHoldingRegisters = 6;              //
+const uint8_t numHoldingRegisters = 14;             //
 uint16_t holdingRegisters[numHoldingRegisters];     //
 
 #include <TMCStepper.h>                             //  ▀▀█▀▀ █▀▄▀█ █▀▀ 
@@ -25,13 +36,13 @@ void setup() {
   modbus.configureHoldingRegisters(holdingRegisters, numHoldingRegisters);  //  █▀▄▀█ █▀▀█ █▀▀▄ █▀▀▄ █░░█ █▀▀ 
   MODBUS_SERIAL.begin(MODBUS_BAUD, MODBUS_CONFIG);                          //  █░▀░█ █░░█ █░░█ █▀▀▄ █░░█ ▀▀█ 
   modbus.begin(MODBUS_UNIT_ID, MODBUS_BAUD, MODBUS_CONFIG);                 //  ▀░░░▀ ▀▀▀▀ ▀▀▀░ ▀▀▀░ ░▀▀▀ ▀▀▀
-  holdingRegisters[0] = 500;                                                //
+  holdingRegisters[0] = 300;                                                //
 
-  SERIAL_PORT.begin(115200);      // HW UART drivers                            ▀▀█▀▀ █▀▄▀█ █▀▀ 
-  driver.toff(5);                 // Enables driver in software                 ░░█░░ █░▀░█ █░░ 
-  driver.rms_current(700);        // Set motor RMS current                      ░░▀░░ ▀░░░▀ ▀▀▀               
-  driver.microsteps(16);          // Set microsteps to 1/16th                   
-  driver.pwm_autoscale(true);     // Needed for stealthChop
+  SERIAL_PORT.begin(115200);                      // HW UART drivers                            ▀▀█▀▀ █▀▄▀█ █▀▀ 
+  driver.toff(5);                                 // Enables driver in software                 ░░█░░ █░▀░█ █░░ 
+  driver.rms_current(holdingRegisters[0]);        // Set motor RMS current                      ░░▀░░ ▀░░░▀ ▀▀▀               
+  driver.microsteps(16);                          // Set microsteps to 1/16th                   
+  driver.pwm_autoscale(true);                     // Needed for stealthChop
 
   stepper.setMaxSpeed(50*steps_per_mm);                                      // █▀▀█ █▀▀ █▀▀ █▀▀ █░░ 
   stepper.setAcceleration(1000*steps_per_mm);                                // █▄▄█ █░░ █░░ █▀▀ █░░ 
@@ -39,15 +50,22 @@ void setup() {
   stepper.setPinsInverted(false, false, true);                               //
 }
 
+  int16_t razn;
 
 void loop() {
-  
-  holdingRegisters[0] = stepper.currentPosition() / steps_per_mm;
-  holdingRegisters[1] = stepper.distanceToGo() / steps_per_mm;
-  holdingRegisters[3] = driver.rms_current();
+  holdingRegisters[10] = stepper.currentPosition() / steps_per_mm;
+  holdingRegisters[11] = stepper.distanceToGo() / steps_per_mm;
+  holdingRegisters[1] = driver.rms_current();
+
+  razn = holdingRegisters[0] - holdingRegisters[1];
+  razn = abs(razn);
+  holdingRegisters[4] = razn;
+  if (razn > 50) {
+    driver.rms_current(holdingRegisters[0]);        // Set motor RMS current
+  }
 
   if (holdingRegisters[2] != stepper.targetPosition() / steps_per_mm) {
-    stepper.moveTo(holdingRegisters[2] * steps_per_mm); // Move 100mm
+    stepper.moveTo(holdingRegisters[12] * steps_per_mm); // Move 100mm
     stepper.enableOutputs();
   }
 
